4c381c1c6b0a48d73036751db6e90b4a2dcfd8bd
Lin Thit Khant <teteterrs@gmail.com>
2023-02-19 21:17:50 +0630

    Initial Commit

---
+++ MC Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC Delivery
+//
+//  Created by Lin Thit Khant on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

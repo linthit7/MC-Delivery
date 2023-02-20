1cf4254484b4348cb637e9dc8d0e0008ef5e08d9
Lin Thit Khant <teteterrs@gmail.com>
2023-02-20 10:45:42 +0630

    Remove storyboard implementation for MCD & MCDC

---
+++ MC Delivery/TabbarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabbarViewController.swift
+//  MC Delivery
+//
+//  Created by Lin Thit Khant on 2/20/23.
+//
+
+import UIKit
+
+class TabbarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
